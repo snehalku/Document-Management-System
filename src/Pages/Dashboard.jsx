ec7953163a00626045f528fb0b03e9c3e9911d59
Snehal <snehalkulkarni009@gmail.com>
2025-04-29 13:45:52 +0530

    cosmetic changes

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -55,7 +55,6 @@ const projectData = [
     transactionName: "Document Uploaded",
     transactionDate: "2025-04-26",
     tooltip: "Click to preview document",
-
     // documentId: "DOC457",
     // documentName: "Agreement #789",
   },
