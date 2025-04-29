ea9f9fe97c3fd28a93c25c001c7feb79832ada04
Snehal <snehalkulkarni009@gmail.com>
2025-04-29 10:14:25 +0530

    Created dashboard and documents table

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -13,36 +13,61 @@ import {
   Stack,
   IconButton,
   Tooltip,
+  FormControl,
+  MenuItem,
+  InputLabel,
+  Select,
+  Button,
 } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 import ArrowDropDown from "@mui/icons-material/ArrowDropDown";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import InfoIcon from "@mui/icons-material/Info"; // for View Details button
 
-
 const pump_report = "your-pdf-url-or-path"; // Replace with your actual file
 const projectData = [
   {
     id: 1,
-    applicationName: "Invoice Processing",
+    documentName: "Invoice",
+    applicationName: "Account",
     transactionId: "TXN123",
     transactionName: "Payment Received",
     transactionDate: "2025-04-25",
+    tooltip: "Click to preview document",
     // documentId: "DOC456",
     // documentName: "Invoice #456",
   },
   {
     id: 2,
-    applicationName: "Document Management",
+    documentName: "Bank Statement",
+    applicationName: "Account",
     transactionId: "TXN124",
     transactionName: "Document Uploaded",
     transactionDate: "2025-04-26",
+    tooltip: "Click to preview document",
+
+    // documentId: "DOC457",
+    // documentName: "Agreement #789",
+  },
+  {
+    id: 3,
+    documentName: "Tax Document",
+    applicationName: "Account",
+    transactionId: "TXN122",
+    transactionName: "Document Uploaded",
+    transactionDate: "2025-04-27",
+    tooltip: "Click to preview document",
+
     // documentId: "DOC457",
     // documentName: "Agreement #789",
   },
 ];
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+
   return (
     <Box
       sx={{
@@ -56,7 +81,7 @@ const Dashboard = () => {
         sx={{
           bgcolor: "#f2f4f5",
           minHeight: "90vh",
-          width: "100vw",
+          width: "100%",
           py: 4,
           pl: "70px",
           pt: "12px",
@@ -70,18 +95,57 @@ const Dashboard = () => {
           variant="h5"
           component="h1"
           fontWeight="bold"
-          sx={{ mb: 2 }}
+          sx={{ mb: 1 }}
         >
-         Documents
+          Documents
         </Typography>
+
+        <Stack
+          direction="row"
+          spacing={2}
+          sx={{ mb: 2 }}
+          justifyContent="flex-start"
+          alignItems="center"
+        >
+          {/* Label */}
+          <Typography variant="subtitle1" fontWeight="bold">
+            Select Application
+          </Typography>
+
+          {/* Select Dropdown using only MUI components */}
+          <FormControl sx={{ minWidth: 200 }}>
+            <Select
+              labelId="application-select-label"
+              id="application-select"
+              defaultValue="Account"
+              label="Application"
+              sx={{
+                bgcolor: "#fff",
+                height: "45px",
+                borderRadius: "5px",
+                boxShadow: "0px 2px 6px rgba(0, 0, 0, 0.25)",
+                "& fieldset": {
+                  border: "none",
+                },
+                "&.Mui-focused": {
+                  boxShadow: "0px 2px 10px rgba(0, 0, 0, 0.35)",
+                },
+              }}
+            >
+              <MenuItem value="Account">Account</MenuItem>
+              <MenuItem value="Finance">Finance</MenuItem>
+              <MenuItem value="Sales">Sales</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
         <Stack
           direction="row"
           spacing={2}
           sx={{ mb: 3 }}
-          justifyContent="space-between"
+          //   justifyContent="space-between"
         >
           <TextField
-            placeholder="Search by All..."
+            placeholder="Search by all parameters..."
             variant="outlined"
             sx={{
               width: "50%",
@@ -100,72 +164,108 @@ const Dashboard = () => {
               },
             }}
           />
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#99caff",
+              width: "110px",
+              color: "black",
+              borderRadius: "10px",
+              fontWeight: "bold",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              "&:hover": {
+                bgcolor: "#88b9ee",
+              },
+            }}
+          >
+            Get Data
+          </Button>
         </Stack>
-    <TableContainer component={Paper} sx={{ mb: 4, borderRadius: "10px 10px 0 0" }}>
-      <Table>
-        <TableHead>
-          <TableRow sx={{ bgcolor: "#99caff" }}>
-            {[
-              "Id",
-              "Application Name",
-              "Transaction ID",
-              "Transaction Name",
-              "Transaction Date",
-              // "Document ID",
-              // "Document Name",
-              "Action",
-            ].map((header) => (
-              <TableCell key={header}>
-                <Stack direction="row" alignItems="center" spacing={1}>
-                  <Typography fontWeight="bold">{header}</Typography>
-                  {header !== "Action" && <ArrowDropDown />}
-                </Stack>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
 
-        <TableBody>
-          {projectData.map((project) => (
-            <TableRow key={project.id} hover>
-              <TableCell>
-                <Typography fontWeight="bold">{project.id}</Typography>
-              </TableCell>
-              <TableCell>{project.applicationName}</TableCell>
-              <TableCell>{project.transactionId}</TableCell>
-              <TableCell>{project.transactionName}</TableCell>
-              <TableCell>{project.transactionDate}</TableCell>
-              {/* <TableCell>{project.documentId}</TableCell>
+        <TableContainer
+          component={Paper}
+          sx={{ mb: 4, borderRadius: "10px 10px 0 0" }}
+        >
+          <Table>
+            <TableHead>
+              <TableRow sx={{ bgcolor: "#99caff" }}>
+                {[
+                  "Id",
+                  "Document Name",
+                  "Application Name",
+                  "Transaction ID",
+                  "Transaction Name",
+                  "Transaction Date",
+                  // "Document ID",
+                  // "Document Name",
+                ].map((header) => (
+                  <TableCell key={header}>
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                      <Typography fontWeight="bold">{header}</Typography>
+                      {header !== "Action" && <ArrowDropDown />}
+                    </Stack>
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {projectData.map((project) => (
+                <TableRow key={project.id} hover>
+                  <TableCell>
+                    <Typography fontWeight="bold">{project.id}</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Tooltip title={project.tooltip} arrow>
+                      <Typography
+                        fontWeight="medium"
+                        sx={{
+                          whiteSpace: "pre-line",
+                          cursor: "pointer",
+                          textDecoration: "none",
+                          color: "#000000",
+                        }}
+                        onClick={() => navigate("/previewDocument")}
+                      >
+                        {project.documentName}
+                      </Typography>
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell>{project.applicationName}</TableCell>
+                  <TableCell>{project.transactionId}</TableCell>
+                  <TableCell>{project.transactionName}</TableCell>
+                  <TableCell>{project.transactionDate}</TableCell>
+                  {/* <TableCell>{project.documentId}</TableCell>
               <TableCell>{project.documentName}</TableCell> */}
 
-              {/* Action Buttons */}
-              <TableCell>
-                <Stack direction="row" spacing={1}>
-                  {/* View Document */}
-                  <Tooltip title="View Document">
-                    <IconButton
-                      color="primary"
-                      onClick={() => window.open(pump_report, "_blank")}
-                    >
-                      <VisibilityIcon />
-                    </IconButton>
-                  </Tooltip>
+                  {/* Action Buttons */}
+                  {/* <TableCell>
+                    <Stack direction="row" spacing={1}>
+                      <Tooltip title="View Document">
+                        <IconButton
+                          color="primary"
+                          onClick={() => window.open(pump_report, "_blank")}
+                        >
+                          <VisibilityIcon />
+                        </IconButton>
+                      </Tooltip>
 
-                  {/* Download Document */}
-                  <Tooltip title="Download Document">
-                    <a
-                      href={pump_report}
-                      download="pump_report.pdf"
-                      style={{ color: "inherit", textDecoration: "none" }}
-                    >
-                      <IconButton color="secondary">
-                        <FileDownloadIcon />
-                      </IconButton>
-                    </a>
-                  </Tooltip>
+                      <Tooltip title="Download Document">
+                        <a
+                          href={pump_report}
+                          download="pump_report.pdf"
+                          style={{ color: "inherit", textDecoration: "none" }}
+                        >
+                          <IconButton color="secondary">
+                            <FileDownloadIcon />
+                          </IconButton>
+                        </a>
+                      </Tooltip>
 
-                  {/* View Details */}
-                  {/* <Tooltip title="View Details">
+                      <Tooltip title="View Details">
                     <IconButton
                       color="info"
                       onClick={() => {
@@ -174,15 +274,15 @@ const Dashboard = () => {
                     >
                       <InfoIcon />
                     </IconButton>
-                  </Tooltip> */}
-                </Stack>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-    </Box>
+                  </Tooltip>
+                    </Stack>
+                  </TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
     </Box>
   );
 };
