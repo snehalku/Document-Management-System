56a7b58587d4d96773361d2a0538a61d63d2488b
Gayu2912 <bhavargg8799@gmail.com>
2025-05-28 12:40:58 +0530

    changes in invoice flow

--- src/Pages/Invoice.jsx
+++ src/Pages/Invoice.jsx
@@ -78,6 +78,7 @@ const Invoice = () => {
   const [showSnackbar, setShowSnackbar] = useState(false);
   const [alertOpen, setAlertOpen] = useState(false);
   const [filterInvoiceNo, setFilterInvoiceNo] = useState("");
+   const [filterInvoiceDate, setFilterInvoiceDate] = useState("");
   const [filterCompanyName, setFilterCompanyName] = useState("");
   const [previewDocPath, setPreviewDocPath] = useState(null);
   const [selectedDate, setSelectedDate] = useState("");
